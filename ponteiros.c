fe5104c1f516d55a477f2d1563719d953b5d2186
Rodrigo Rocha <rrsilva1701@gmail.com>
2025-10-27 18:24:09 -0300

    estudo ponteiros em c

--- ponteiros.c
+++ ponteiros.c
@@ -4,17 +4,24 @@ int main()
 {
     int x = 10;
     int *p = &x; // a variavel 'p' do tipo ponteiro (*) recebe o endereço de 'x'
+    // '&' = endereço na memoria
+    // '*' = declarar o ponteiro, acessar e modificar o valor no endereço
 
     printf("Valor de x: %d\n", x);
     printf("Endereço de x: %p\n", &x);
     printf("Conteudo de p (o endereço): %p\n", p);
     printf("Valor apontado por p: %d\n", *p);
 
+    *p = 20; // modificando valor atraves do ponteiro
+
+    printf("Valor de x: %d\n", x);
+
     // saida:
     // Valor de x: 10
     // Endereço de x: 0x7fffee5070bc
     // Conteudo de p (o endereço): 0x7fffee5070bc
     // Valor apontado por p: 10
+    // Valor de x: 20
 
     return 0;
 }
